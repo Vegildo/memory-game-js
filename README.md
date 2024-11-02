79df860b0af6968b616b6d1c918a9855c6c18220
Leonardo Braga <88868926+Vegildo@users.noreply.github.com>
2024-11-02 19:28:33 -0300

    :rocket: feat: Uploading dos arquivos do projeto

---
+++ README.md
@@ -0,0 +1,35 @@
+## Bem-vindo(a)
+Olá, meu nome é Leonardo Braga. E esse é o desafio para o bootcamp da RiHappy + DIO, para Front-End.
+
+## Awesome JSGame Memory
+
+
+
+Bem-vindo ao **JSGame MEMORY**! Neste repositório, você encontrará um jogo divertido de memória. Este projeto não apenas oferece entretenimento, mas também demonstra várias técnicas avançadas de desenvolvimento de jogos em JavaScript.
+
+### Tecnologias Utilizadas
+
+- HTML5 e CSS3 para a estrutura e aparência do jogo.
+- JavaScript para a lógica de programação, manipulação do DOM e interatividade.
+
+### Funcionalidades Incríveis
+
+- **Sistema de alarme de vitória**: Receba uma mensagem quando completar o jogo!
+
+### Como Jogar
+
+1. Clone este repositório para sua máquina local.
+2. Abra o arquivo `index.html` em seu navegador web.
+3. Clique nas caixas e memorize os emois de animais.
+4. Divirta-se!
+
+
+### Créditos
+
+Este jogo foi desenvolvido, por Leonardo Braga, como parte de um projeto educacional da Digital Innovation One.
+
+---
+
+Divirta-se jogando o **JSGame MEMORY** enquanto explora as técnicas modernas de desenvolvimento de jogos em JavaScript. Lembre-se de conferir o repositório original [aqui](https://github.com/Vegildo/memory-game-js) e deixar uma ⭐️ se você gostou do projeto!
+
+
---
+++ src/scripts/engine.js
@@ -0,0 +1,61 @@
+const emojis = [
+    "🐱",
+    "🐱",
+    "🦝",
+    "🦝",
+    "🦊",
+    "🦊",
+    "🐶",
+    "🐶",
+    "🐵",
+    "🐵",
+    "🦁",
+    "🦁",
+    "🐯",
+    "🐯",
+    "🐮",
+    "🐮",
+  ];
+  let openCards = [];
+  
+  let shuffleEmojis = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1)); // Explicar essa linha de código: 
+  
+  for (let i = 0; i < emojis.length; i++) {
+    let box = document.createElement("div");
+    box.className = "item";
+    box.innerHTML = shuffleEmojis[i];
+    box.onclick = handleClick;
+    document.querySelector(".game").appendChild(box);
+  }
+  
+  function handleClick() {
+    if (openCards.length < 2) {
+      this.classList.add("boxOpen");
+      openCards.push(this);
+    }
+  
+    if (openCards.length == 2) {
+      setTimeout(checkMatch, 500);
+    }
+  
+    console.log(openCards);
+  }
+  
+  function checkMatch() {
+    if (openCards[0].innerHTML === openCards[1].innerHTML) {
+      openCards[0].classList.add("boxMatch");
+      openCards[1].classList.add("boxMatch");
+    } else {
+      openCards[0].classList.remove("boxOpen");
+      openCards[1].classList.remove("boxOpen");
+    }
+  
+    openCards = [];
+  
+    if (document.querySelectorAll(".boxMatch").length === emojis.length) {
+    //   alert("Você venceu !");
+        // Mensagem de vitória estilizada, apagando a antiga:        
+        const victory = document.querySelector(".victory");        
+        victory.innerHTML = "Parabéns! Você venceu!";
+    }
+  }
